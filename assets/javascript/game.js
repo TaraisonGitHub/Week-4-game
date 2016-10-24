acfe009de92e373cfbf6149037f06c1639c23b1d
Tara Baker <Tara Baker>
2016-10-23 21:58:41 -0400

    initial html and css styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+var lukeSkywalker = {
+	"Luke Skywalker",
+	"health: 140",
+}
+
+var bobaFett = {
+	"name: Boba Fett",
+	"health: 120"
+}
+
+var princessLeia = {
+	"name: Princess Leia",
+	"health: 160"
+}
+
+var darthVader = {
+	"name: Darth Vader",
+	"health: 180"
+}
+
+$(document).onload(function() {
+
+var n = $(".person. value").html("hello");
+	console.log(n);
+});
